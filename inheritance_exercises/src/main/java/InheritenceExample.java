831e7538a68729881fc4593b4cbdfadaf02d4585
Hyunjay Lee <hyunjay_lee@hcl.com>
2021-06-29 17:21:06 -0400

    added .gitignore

--- inheritance_exercises/src/main/java/InheritenceExample.java
+++ inheritance_exercises/src/main/java/InheritenceExample.java
@@ -4,19 +4,18 @@ public class InheritenceExample {
 	public static void main(String[] args) {
 		System.out.println("Bike example:");
 		Bike M = new Bike();
-		
 		System.out.println("\nDog example:");
 		Dog dog = new Dog();
 		dog.walk();
 		dog.eat();
 		dog.bark();
 		dog.makeNoise("Food!");
-		
+
 		Animal animal = new Animal();
 		animal.makeNoise("Hello");
-		
+
 		System.out.println("\nA and B example:");
-		A a = new A("Albert",3);
+		A a = new A("Albert", 3);
 		B b = new B("Sam", 20, "Student");
 		a.display();
 		b.display();
